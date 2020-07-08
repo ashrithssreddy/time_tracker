d3cd1b873c8f9396db15340e708ca358183cf63c
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-08 14:36:36 +0530

    start time of each 10 min interval

--- code_read_input.sh
+++ code_read_input.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+: ' Determining the start time of 10 minute periods'
+time_30_mins_ago=$(date -d '30 minutes ago' '+%Y-%m-%d %H:%M')
+time_20_mins_ago=$(date -d '20 minutes ago' '+%Y-%m-%d %H:%M')
+time_10_mins_ago=$(date -d '10 minutes ago' '+%Y-%m-%d %H:%M')
+time_0_mins_ago=$(date -d '0 minutes ago' '+%Y-%m-%d %H:%M')
+
 : ' Determining the file to write to'
 system_date_filename=$(date '+%Y-%m-%d')
 system_date_filename+=".txt"
