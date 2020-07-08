d5ec7ef887f341553360edbf94051e09707d3bc1
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-08 14:38:45 +0530

    completed the body of if-else blocks

--- code_read_input.sh
+++ code_read_input.sh
@@ -19,10 +19,18 @@ echo $duration
 if [ $duration == 10 ]
 then 
 	echo "Enter data at 10 minute interval"
-
+	read -p "Enter what you did from $time_30_mins_ago to $time_20_mins_ago: " task_10_mins_1
+	read -p "Enter what you did from $time_20_mins_ago to $time_10_mins_ago: " task_10_mins_2
+	read -p "Enter what you did from $time_10_mins_ago to $time_0_mins_ago: " task_10_mins_3
+	echo "$time_30_mins_ago to $time_20_mins_ago: $task_10_mins_1" >> $system_date_filename
+	echo "$time_20_mins_ago to $time_10_mins_ago: $task_10_mins_2" >> $system_date_filename
+	echo "$time_10_mins_ago to $time_0_mins_ago: $task_10_mins_3" >> $system_date_filename
 else 
 	echo "Enter data at 30 minute interval"
-	
+	read -p "Enter what you did from $time_30_mins_ago to $time_0_mins_ago: " task_30_mins
+	echo "$time_30_mins_ago to $time_20_mins_ago: $task_30_mins" >> $system_date_filename
+	echo "$time_20_mins_ago to $time_10_mins_ago: $task_30_mins" >> $system_date_filename
+	echo "$time_10_mins_ago to $time_0_mins_ago: $task_30_mins" >> $system_date_filename
 fi
 
 $SHELL
