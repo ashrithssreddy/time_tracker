5d955e81bc95fcc4cdc6eb47c7645fecc2ede58e
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-08 14:35:25 +0530

    filename to write to

--- code_read_input.sh
+++ code_read_input.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+: ' Determining the file to write to'
 system_date_filename=$(date '+%Y-%m-%d')
 system_date_filename+=".txt"
-echo "apple a day" >> $system_date_filename
+
+
+
+
+
+
+
 $SHELL
