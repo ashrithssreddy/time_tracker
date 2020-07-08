297cf56c0cbc221059b6422e414705b26f49ad4e
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-08 14:37:51 +0530

    added if-else blocks

--- code_read_input.sh
+++ code_read_input.sh
@@ -10,10 +10,19 @@ time_0_mins_ago=$(date -d '0 minutes ago' '+%Y-%m-%d %H:%M')
 system_date_filename=$(date '+%Y-%m-%d')
 system_date_filename+=".txt"
 
+read -p "Duration: " duration
+echo $duration
 
+read -p "Duration: " duration
+echo $duration
 
+if [ $duration == 10 ]
+then 
+	echo "Enter data at 10 minute interval"
 
-
-
+else 
+	echo "Enter data at 30 minute interval"
+	
+fi
 
 $SHELL
