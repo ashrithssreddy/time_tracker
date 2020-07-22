4890686fb646dae283469007d24014b52606c65b
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-22 10:33:12 +0530

    Minor changes

--- open_file_manually.sh
+++ open_file_manually.sh
@@ -1,20 +1,22 @@
 : ' Determine file name'
 system_date_filename=$(date '+%Y-%m-%d')
 system_date_filename+=".txt"
-system_date_filename="logs/${system_date_filename}"
+#system_date_filename="logs/${system_date_filename}"
 
-cd "C:\ashrith\98_open_source_development\ad-hoc-projects\time_tracker"
+cd "C:\Users\Ashrith Reddy\Google Drive\time_tracker_logs"
 
 # Open the file if it exists
 if test -f "$system_date_filename"; then
 	start $system_date_filename
 else
 # If file does not exist already, create a file using template
-	cp "logs/00_template.txt" $system_date_filename
+	cp "00_template.txt" $system_date_filename
 	start $system_date_filename
 	
 	# Open previous day's tracker
-	start $PWD
+	# start "${PWD}"
+	start .
+	start "final_results.xlsx"
 	# previous_date_filename=$(date -d '1 day ago' '+%Y-%m-%d')
 	# previous_date_filename+=".txt"	
 fi
