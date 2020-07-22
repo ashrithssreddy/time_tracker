e58dac110a3216bdecbc13050cdc4c6d86343dd6
Ashrith Reddy <ashrithssreddy@gmail.com>
2020-07-22 10:49:52 +0530

    fixed comments

--- 04_generate_time_report/results_collation.R
+++ 04_generate_time_report/results_collation.R
@@ -1,48 +1,47 @@
+#### Objective: Calculate the time wasted in a day using "wasted" keyword #### 
+
 library("dplyr")
-rm(list=ls())
-cat("\014")
+rm(list=ls()); cat("\014")
 
-# setwd("logs")
-# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-setwd("C:/Users/Ashrith Reddy/Google Drive/time_tracker_logs/logs")
+# Set working directory to folder containing log files
+# Ensure log files are the only .txt files in this folder.
+setwd("C:/Users/Ashrith Reddy/Google Drive/time_tracker_logs")
+
+# Derive list of all text files. 
 file_names = dir(pattern = ".txt")
-file_names = file_names[file_names != "00_template.txt"]
 
-# file = file_names[2]
+# Iterate over files to collect/collate results
+# Empty dataframe ot collect final_results
 final_results = data.frame(stringsAsFactors = F)
 for(file in file_names){
-  print(file)
   
+  # Each line of the file to be a row in data.frame 
   text = readLines(file) %>% 
-    stringr::str_squish() %>% 
+    stringr::str_squish() %>% # Remove empty spaces 
     as.data.frame() %>% 
     setNames("text")
   
+  # Filter for line items pertaining to "wasted" time
   text = text %>% 
-    filter(text != "") %>% 
+    filter(text != "") %>%  # Remove blank lines
     mutate(text = tolower(text)) %>% 
-    # tidyr::separate(col = text, into = c("time", "activity"), sep = " : ") %>% 
     filter(grepl(text, pattern = "wasted"))
   
+  # Convert count of line-items to hours
   wasted_time_hours = text %>% 
     nrow() %>% 
     `*`(15/60)
   
-  one_day_productivity = data.frame(calendar_date = file %>% stringr::str_replace(".txt",""), wasted_time_hours, 
+  # Collect one day's productivity into final dataframe
+  one_day_productivity = data.frame(calendar_date = stringr::str_replace(file, ".txt",""), 
+                                    wasted_time_hours, 
                                     stringsAsFactors = F)
   final_results = final_results %>% rbind(one_day_productivity)
 }
 
+# Format and write final collated dataset
 final_results %>% 
   arrange(desc(calendar_date)) %>% 
   # mutate(calendar_date = as.Date(calendar_date)) %>% 
   # mutate(week_id = lubridate::week(calendar_date)) %>% 
   openxlsx::write.xlsx("final_results.xlsx")
-
-# q('no')
-
-# strftime(c("2020-01-01","2020-01-02", "2020-01-05", "2020-01-06"), format = "%V")
-# %>% 
-
-# Next Steps
-# Replace  == wasted with column containing wasted
